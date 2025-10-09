4414bddacacea7d8ccb49cdd268fa65ca3f2a360
Christopher Torgalson <manager@bedlamhotel.com>
2025-10-08 21:25:28 -0400

    feat: move default grid data out to _data/ for use by 11ty, Preact

---
+++ src/_data/gridDefaults.js
@@ -0,0 +1,11 @@
+export default {
+  namespace: "gs",
+  breakpointDesktop: "width >= 60rem",
+  breakpointTablet: "width >= 48rem",
+  columnGapDesktop: "var(--column-gap-desktop, 1.5rem)",
+  columnGapTablet: "var(--column-gap-tablet, 1.125rem)",
+  rowGapDesktop: "var(--row-gap-desktop, 1.5lh)",
+  rowGapMobile: "var(--row-gap-mobile, 1lh)",
+  rowGapTablet: "var(--row-gap-tablet, 0.75lh)",
+  columns: 12,
+};
--- src/components/GridSystem.jsx
+++ src/components/GridSystem.jsx
@@ -9,20 +9,7 @@ import { gridCssTemplate } from "../lib/grid/gridCssTemplate";
 import "water.css/out/water.min.css";
 import "../assets/css/styles.css";
 
-export default function GridSystem({
-  readOnly = {},
-  defaultState = {
-    namespace: "gs",
-    breakpointDesktop: "width >= 60rem",
-    breakpointTablet: "width >= 48rem",
-    columnGapDesktop: "var(--column-gap-desktop, 1.5rem)",
-    columnGapTablet: "var(--column-gap-tablet, 1.125rem)",
-    rowGapDesktop: "var(--row-gap-desktop, 1.5lh)",
-    rowGapMobile: "var(--row-gap-mobile, 1lh)",
-    rowGapTablet: "var(--row-gap-tablet, 0.75lh)",
-    columns: 12,
-  },
-}) {
+export default function GridSystem({ readOnly = {}, defaultState }) {
   const [state, setState] = useState(() => ({
     ...defaultState,
     ...Object.fromEntries(new URLSearchParams(window.location.search)),
